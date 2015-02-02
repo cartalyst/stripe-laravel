563d715dd21a2794f1b311913ca39541c165e2fb
Bruno Gaspar <brunofgaspar1@gmail.com>
2015-02-02 19:03:27 +0000

    Add facade test.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ tests/FacadeTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Part of the Stripe Laravel package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe Laravel
+ * @version    1.0.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2015, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Tests;
+
+use ReflectionClass;
+use PHPUnit_Framework_TestCase;
+
+class FacadeTest extends PHPUnit_Framework_TestCase {
+
+	/** @test */
+	public function it_can_test_it_is_a_facade()
+	{
+		$facade = new ReflectionClass('Illuminate\Support\Facades\Facade');
+
+		$reflection = new ReflectionClass('Cartalyst\Stripe\Laravel\Stripe');
+
+		$this->assertTrue($reflection->isSubclassOf($facade));
+	}
+
+	/** @test */
+	public function it_can_test_it_is_a_facade_accessor()
+	{
+		$reflection = new ReflectionClass('Cartalyst\Stripe\Laravel\Stripe');
+
+		$method = $reflection->getMethod('getFacadeAccessor');
+		$method->setAccessible(true);
+
+		$this->assertEquals('stripe', $method->invoke(null));
+	}
+
+}
