5d70734deff3bd59c5069c16abbb454c92f6635b
Bruno Gaspar <brunofgaspar1@gmail.com>
2017-01-24 22:10:14 +0000

    Bump copyright year.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- tests/FacadeTest.php
+++ tests/FacadeTest.php
@@ -14,7 +14,7 @@
  * @version    6.0.0
  * @author     Cartalyst LLC
  * @license    BSD License (3-clause)
- * @copyright  (c) 2011-2016, Cartalyst LLC
+ * @copyright  (c) 2011-2017, Cartalyst LLC
  * @link       http://cartalyst.com
  */
 
